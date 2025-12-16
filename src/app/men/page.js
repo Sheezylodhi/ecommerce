71f8b656a09aa150bb0778e5a6c5a49c58ae2b0a
Sheezylodhi <muhammadshahzaiblodhi@gmail.com>
2025-12-16 22:39:44 +0500

    ,,

---
+++ src/app/men/page.js
@@ -0,0 +1,12 @@
+"use client"; // agar client side component hai
+
+import React from "react";
+
+export default function MenPage() {
+  return (
+    <div>
+      <h1>Men's Section</h1>
+      {/* Tumhara content */}
+    </div>
+  );
+}
